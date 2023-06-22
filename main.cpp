699146b0667e8c00a8a9813fe3cf8eaf76a60669
YannosK <giannos2405@gmail.com>
2023-06-22 11:03:54 +0300

    Having 8 contact closure. Could optimize w functs

--- main.cpp
+++ main.cpp
@@ -24,21 +24,45 @@ EthernetClient client;
 
 bool printWebData = true;  // will print everything the client gets, if true
 
-//setting up the button
-int pin_snap1 = 7;
-int pin_snap2 = 8;
+//setting up the buttons
+int pin_snap1 = 1;
+int pin_snap2 = 2;
+int pin_snap3 = 3;
+int pin_snap4 = 4;
+int pin_snap5 = 5;
+int pin_snap6 = 6;
+int pin_snap7 = 7;
+int pin_snap8 = 8;
 int val1 = 0;
 int val2 = 0;
+int val3 = 0;
+int val4 = 0;
+int val5 = 0;
+int val6 = 0;
+int val7 = 0;
+int val8 = 0;
 int oldval1=0;
 int oldval2=0;
+int oldval3=0;
+int oldval4=0;
+int oldval5=0;
+int oldval6=0;
+int oldval7=0;
+int oldval8=0;
 
 
 void setup() {
   Serial.begin(115200);
 
-  // Setup the pin to read
+  // Setup the pins to read
    pinMode(pin_snap1, INPUT);
    pinMode(pin_snap2, INPUT);
+   pinMode(pin_snap3, INPUT);
+   pinMode(pin_snap4, INPUT);
+   pinMode(pin_snap5, INPUT);
+   pinMode(pin_snap6, INPUT);
+   pinMode(pin_snap7, INPUT);
+   pinMode(pin_snap8, INPUT);
 
   // start the Ethernet connection:
   Ethernet.begin(mac, ip, gateway, subnet);
@@ -75,6 +99,12 @@ void loop() {
   
   val1 = digitalRead(pin_snap1);
   val2 = digitalRead(pin_snap2);
+  val3 = digitalRead(pin_snap3);
+  val4 = digitalRead(pin_snap4);
+  val5 = digitalRead(pin_snap5);
+  val6 = digitalRead(pin_snap6);
+  val7 = digitalRead(pin_snap7);
+  val8 = digitalRead(pin_snap8);
   
   if (oldval1 != val1) {
     oldval1 = val1;
@@ -84,11 +114,59 @@ void loop() {
     }
   }
 
-  if (oldval2 != val2) {    
+  if (oldval2 != val2) {
     oldval2 = val2;
     if (val2 == 1) {
       client.println("LOADSNAPSHOT^Show 1^Snap 1.2");
-      Serial.println(val2);
+      Serial.println("Snap 1.2");
+    }
+  }
+
+  if (oldval3 != val3) {
+    oldval3 = val3;
+    if (val3 == 1) {
+      client.println("LOADSNAPSHOT^Show 1^Snap 1.3");
+      Serial.println("Snap 1.3");
+    }
+  }
+
+  if (oldval4 != val4) {
+    oldval4 = val4;
+    if (val4 == 1) {
+      client.println("LOADSNAPSHOT^Show 1^Snap 1.4");
+      Serial.println("Snap 1.4");
+    }
+  }
+
+  if (oldval5 != val5) {
+    oldval5 = val5;
+    if (val5 == 1) {
+      client.println("LOADSNAPSHOT^Show 1^Snap 1.5");
+      Serial.println("Snap 1.5");
+    }
+  }
+
+  if (oldval6 != val6) {
+    oldval6 = val6;
+    if (val6 == 1) {
+      client.println("LOADSNAPSHOT^Show 1^Snap 1.6");
+      Serial.println("Snap 1.6");
+    }
+  }
+
+  if (oldval7 != val7) {
+    oldval7 = val7;
+    if (val7 == 1) {
+      client.println("LOADSNAPSHOT^Show 1^Snap 1.7");
+      Serial.println("Snap 1.7");
+    }
+  }
+
+  if (oldval8 != val8) {
+    oldval8 = val8;
+    if (val8 == 1) {
+      client.println("LOADSNAPSHOT^Show 1^Snap 1.8");
+      Serial.println("Snap 1.8");
     }
   }
 }
